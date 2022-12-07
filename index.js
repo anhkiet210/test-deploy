0769c8e28d5f9d80f0a9b8b0ea96be463ff9122c
anhkiet210 <anhkietk053@gmail.com>
2022-12-07 22:26:45 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("tracuusaubenh-Thấu hiểu cây trồng");
+});
+
+const port = 5000;
+app.listen(port, () => console.log(`Connect successfully to ${port}!`));
