9ca9c5381fce31bca8d6d3db313fc7cbe2840cbf
anhkiet210 <anhkietk053@gmail.com>
2022-12-09 16:27:36 +0700

    test deploy v1

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,17 +1,26 @@
-import * as UserController from "../app/http/controllers/user.controller";
+import {
+  Register,
+  Login,
+  getAllUser,
+  getInfoUser,
+  updateInfo,
+  changeAvatar,
+  changePassword,
+  deleteUser,
+} from "../app/http/controllers/user.controller";
 import isAuth from "../app/http/middlewares/auth.middleware";
 import isAuthAdmin from "../app/http/middlewares/authAdmin.middleware";
 import express from "express";
 
 const UserRouter = express.Router();
 
-UserRouter.post("/register", UserController.Register);
-UserRouter.post("/login", UserController.Login);
-UserRouter.get("/get-info-user", isAuth, UserController.getInfoUser);
-UserRouter.put("/update-info", isAuth, UserController.updateInfo);
-UserRouter.put("/change-password", isAuth, UserController.changePassword);
-UserRouter.put("/change-avatar", isAuth, UserController.changeAvatar);
-UserRouter.delete("/delete/:id", isAuthAdmin, UserController.deleteUser);
-UserRouter.get("/", isAuthAdmin, UserController.getAllUser);
+UserRouter.post("/register", Register);
+UserRouter.post("/login", Login);
+UserRouter.get("/get-info-user", isAuth, getInfoUser);
+UserRouter.put("/update-info", isAuth, updateInfo);
+UserRouter.put("/change-password", isAuth, changePassword);
+UserRouter.put("/change-avatar", isAuth, changeAvatar);
+UserRouter.delete("/delete/:id", isAuthAdmin, deleteUser);
+UserRouter.get("/", isAuthAdmin, getAllUser);
 
 export default UserRouter;
